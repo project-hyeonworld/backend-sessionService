90ae20372baf84a7933e0ffb653078ce2db6596c
hyeonwoody <hyeonwoody@gmail.com>
2024-12-06 15:08:43 +0900

    🎨REFACTOR : Update generic `KafkaProducerStrategies` as first class collection for `KafkaProducerStrategy`

--- src/main/java/io/sessionservice/common/event/kafka/producer/GenericKafkaProducerStrategies.java
+++ src/main/java/io/sessionservice/common/event/kafka/producer/GenericKafkaProducerStrategies.java
@@ -2,6 +2,8 @@ package io.sessionservice.common.event.kafka.producer;
 
 import io.sessionservice.common.event.CustomEvent;
 import java.util.List;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 /**
  * @author : hyeonwoody@gmail.com
@@ -10,8 +12,14 @@ import java.util.List;
 public abstract class GenericKafkaProducerStrategies<E extends CustomEvent> implements KafkaProducerStrategy {
 
     protected List<GenericKafkaProducerStrategy<? extends E, ?, ?>> strategies;
+    protected final Executor executor;
 
     public GenericKafkaProducerStrategies(List<GenericKafkaProducerStrategy<? extends E, ?, ?>> kafkaProducerStrategies) {
         strategies = kafkaProducerStrategies;
+        executor = Executors.newFixedThreadPool(strategies.size());
+    }
+
+    public int size() {
+        return strategies.size();
     }
 }
