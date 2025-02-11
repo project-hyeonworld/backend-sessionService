ae41978594c0183b27a4c069151c2a912019b8d5
hyeonwoody <hyeonwoody@gmail.com>
2025-02-12 00:59:04 +0900

    ⚙️CONFIG : Encapsulate application.yml

---
+++ src/main/java/io/sessionservice/common/mapper/UUIDGenerator.java
@@ -0,0 +1,11 @@
+package io.sessionservice.common.mapper;
+
+/**
+ * @author : hyeonwoody@gmail.com
+ * @since : 24. 12. 12.
+ */
+public class UUIDGenerator {
+    public static String generateHourlyID() {
+        return null;
+    }
+}
